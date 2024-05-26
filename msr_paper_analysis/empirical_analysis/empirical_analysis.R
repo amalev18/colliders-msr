9e13916df0e16b12c822eaa45adbd2a3f5a9a141
amalev18 <amandalevinsson@MacBook-Pro-som-tillhor-Amanda.local>
2024-05-26 21:10:52 +0200

    Added R file for empirical analysis

---
+++ msr_paper_analysis/empirical_analysis/empirical_analysis.R
@@ -0,0 +1,65 @@
+library(simstudy)
+library(brms)
+library(bayesplot)
+library(posterior)
+
+# Loading Data 
+#########################
+script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(script_dir)
+dataset <- read.csv('data/commit_level_features_w_test_transformed_small.csv')
+dataset
+
+# Number of samples
+percentage <- 0.1
+n <- percentage*nrow(dataset)
+
+# Retrieve the number of samples wanted from the empirical data
+sampled_data <- dataset %>% 
+  sample_n(n, replace = FALSE)
+
+
+dlist_emp_standardized <- list(
+  TR = sampled_data$TR,
+  SR = sampled_data$SR,
+  R_LOC = standardize(sampled_data$rightLocationDiff),
+  R_Locations = standardize(sampled_data$rightLocationCount)
+)
+
+
+# Creating Models
+#########################
+
+# Without inclusion of collider
+emp_no_collider <- brm(TR ~ SR, family = bernoulli, 
+                       data = dlist_emp_standardized, cores = 4, chains = 4, iter = 10000,
+                       prior = c(prior(normal(-1, 1), class = Intercept), 
+                                 prior(normal(0, 0.05), class = b)))
+
+
+
+# With inclusion of collider
+emp_collider <- brm(TR ~ SR + R_LOC + R_Locations, 
+                    family = bernoulli, data = dlist_emp_standardized, cores = 4, chains = 4, iter = 10000,
+                    prior = c(prior(normal(-1, 1), class = Intercept), 
+                              prior(normal(0, 0.05), class = b)))
+
+
+# Resulting Estimates
+#########################
+summary(emp_no_collider)
+summary(emp_collider)
+
+# Diagnostics: Traceplots
+#########################
+draws_emp_no_collider <- as_draws_df(emp_no_collider)
+draws_emp_collider <- as_draws_df(emp_collider)
+
+mcmc_trace(draws_emp_no_collider)
+mcmc_trace(draws_emp_collider)
+
+
+# Posterior Predictive Checks
+#########################
+pp_check(emp_no_collider, type = "bars", ndraws = 100)
+pp_check(emp_collider, type = "bars", ndraws = 100)
